62ac8e9ebcbffa8c00ebaae00df8d4393deb0d86
Maksym Husak <Maksym_Husak@epam.com>
2023-10-13 21:21:09 +0300

    New repo first commit

---
+++ HomeWork2.1/HomeWork2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+Console.Write("Write down array`s count:\t");
+int ElementCount = int.Parse(Console.ReadLine());
+int NumberOfElements =0;
+
+int finedNumbers(Array randomarray)
+{
+    foreach( int element in randomarray)
+    {
+        if (element > -100 && element < 100)
+        {
+            NumberOfElements++;
+        }
+    } 
+    return NumberOfElements;
+}
+
+int[] Narray = new int[ElementCount];
+for (int i = 0; i < Narray.Length; i++)
+{
+    Console.Write($"Write down {i+1} array`s element:\t");
+    Narray[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine($"Number of elements from -100 to 100 is:\t{NumberOfElements = finedNumbers(Narray)}");
---
+++ HomeWork2.2/HomeWork2.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+int[] aArray = new int[20];
+int[] bArray = new int[20];
+int min, max, count;
+count = 0;
+Console.Write("Write down Min value for number range:\t");
+min = Convert.ToInt32(Console.ReadLine());
+//Int32.TryParse(Console.ReadLine(), out min);
+Console.Write("Write down Max value for number range:\t");
+max = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < 20; i++)
+{
+    switch (i)//Використав switch для практики 
+    {
+        case 0:
+            aArray[i] = new Random().Next(min, max);
+            Console.WriteLine("Array A[20]:");
+            Console.WriteLine($"Element number{i + 1}:\t{aArray[i]}");
+            break;
+        case > 0 and < 19:
+            aArray[i] = new Random().Next(min, max);
+            Console.WriteLine($"Element number{i + 1}:\t{aArray[i]}");
+            break;
+        case 19:
+            aArray[i] = new Random().Next(min, max);
+            Console.WriteLine($"Element number{i + 1}:\t{aArray[i]}");
+            Console.WriteLine("Next array is array B[20], sorted in descending order:");
+            break;
+    }
+
+
+}
+foreach (int element in aArray)
+{
+    if (element <= 888)
+    {
+        bArray[count] = element;
+        count++;
+    }
+}
+Array.Sort(bArray, (x,y)=>y.CompareTo(x));//Сортування массиву B[20]
+Console.WriteLine(String.Join(", ", bArray));//Вивід массиву одним рядком через кому
