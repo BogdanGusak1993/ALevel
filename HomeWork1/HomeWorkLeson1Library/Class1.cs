c5d06e5902cf6058b4dd9147315ba3483c8e1927
Maksym Husak <Maksym_Husak@epam.com>
2023-10-16 13:35:22 +0300

    Re.Commit HW1

---
+++ HomeWork1/HomeWorkLeson1Library/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HomeWorkLeson1Library
+{
+    public  static class Class1
+    {
+        public static bool StartWithUpper(this string? str) {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
