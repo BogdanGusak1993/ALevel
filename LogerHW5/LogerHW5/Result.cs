5bbb34f8610991c43223505ab6f88004ab9d1013
Maksym Husak <Maksym_Husak@epam.com>
2023-10-28 01:29:54 +0300

    Loger(

---
+++ LogerHW5/LogerHW5/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogerHW5
+{
+    internal class Result : Action
+    {
+        bool _status;
+        public string _error;
+        public void ChangeResult(bool status)
+        {
+            if (status)
+            {
+                _status = status;
+            }
+            else
+            {
+                _status = status;
+                _error = "I broke a logic";
+            }
+        }
+    }
+}
