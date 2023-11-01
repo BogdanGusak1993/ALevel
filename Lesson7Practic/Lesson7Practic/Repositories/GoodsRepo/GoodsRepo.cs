944c304c16dbba259d8ef8e459f5acf69a3be346
Maksym Husak <Maksym_Husak@epam.com>
2023-11-01 16:11:44 +0200

    Start of online store)

---
+++ Lesson7Practic/Lesson7Practic/Repositories/GoodsRepo/GoodsRepo.cs
@@ -0,0 +1,30 @@
+﻿using Lesson7Practic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7Practic.Repositories.GoodsRepo
+{
+    internal class GoodsRepo : IGoodsRepo
+    {
+        public  Goods[] GoodsForSale(string[] array) 
+        {
+            Goods[] listOfGoods = new Goods[array.Length];
+            int minPrice = 100;
+            int maxPrice = 1000;
+            int count = 0;
+            foreach (var item in array)
+            {
+                Goods goods = new Goods();
+                goods.name = item;
+                goods.price = new Random().Next(minPrice, maxPrice);
+                goods.quantity = new Random().Next(minPrice, maxPrice);
+                listOfGoods[count] = goods;
+                count++;
+            }
+            return listOfGoods;
+        }
+    }
+}
---
+++ Lesson7Practic/Lesson7Practic/Repositories/UsersRepo/UserRepo.cs
@@ -0,0 +1,22 @@
+﻿using Lesson7Practic.Models;
+using System.IO;
+
+
+namespace Lesson7Practic.Repositories.UsersRepo
+{
+    public class UserRepo : IUserRepo
+    {
+        public User AddUser(string name, string password)
+        {
+            User user = new User();
+
+            user.Name = name;
+            user.Password = password;
+            user.Id = Guid.NewGuid();
+
+            PutUserDataInXML.LoadDataToXml(user);
+
+            return user;
+        }
+    }
+}
