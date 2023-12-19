c34c044fa6382e4d4dc82b1e6ed4a5ffeac929c5
Maksym Husak <Maksym_Husak@epam.com>
2023-12-19 18:12:04 +0200

    HomeWork 4.1

---
+++ HW_4_1/HttpLesson/Service/Abstraction/IRegistrationService.cs
@@ -0,0 +1,15 @@
+﻿using HttpLesson.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpLesson.Service.Abstraction
+{
+    public interface IRegistrationService
+    {
+       Task<LoginData> Registration(string email, string password = null);
+        Task<LoginData> LoginIn(string email, string password = null);
+    }
+}
