207e36e7895e7c1c3ae464d077a7160bff7f772c
Maksym Husak <Maksym_Husak@epam.com>
2024-02-20 17:50:39 +0200

    Up-date

--- Modul_6_1/eShop-Sample2/Catalog/Catalog.Host/Products.cs
+++ Modul_6_1/eShop-Sample2/Catalog/Catalog.Host/Products.cs
@@ -1,6 +1,12 @@
-﻿namespace Catalog.Host
+﻿namespace Catalog.Host;
+
+public class Products
 {
-    public class Products
-    {
-    }
+    public string? Name { get; set; }
+
+    public int Cost { get; set; }
+
+    public string? Brand { get; set; }
+
+    public string? Size { get; set; }
 }
